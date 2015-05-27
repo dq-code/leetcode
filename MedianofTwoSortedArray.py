94282e135b64b834a37d9e536abff93870078dc3
qianden <qianden@cisco.com>
2015-05-27 10:03:37 -0700

    update median of two sorted array

--- MedianofTwoSortedArray.py
+++ MedianofTwoSortedArray.py
@@ -3,52 +3,59 @@
 import math
 
 class MedianofTwoSortedArray:
-    def findMedian(self, arr1, arr2):
-        if len(arr1) == 0 and len(arr2) == 0:
+    def findMedian(self, nums1, nums2):
+        if len(nums1) == 0 and len(nums2) == 0:
             return 0
-        if len(arr1) == 0:
-            mid = int(math.floor(float(len(arr2)-1)/2))
+        if len(nums1) == 0:
+            mid = int(math.floor(float(len(nums2)-1)/2))
             return arr2[mid]
-        if len(arr2) == 0:
-            mid = int(math.floor(float(len(arr1)-1)/2))
-            return arr1[mid]
+        if len(nums2) == 0:
+            mid = int(math.floor(float(len(nums1)-1)/2))
+            return nums1[mid]
         
-        num = int(math.ceil(float(len(arr1)+len(arr2))/2))
-        if(len(arr1)<=len(arr2)):
-            pos = self.helper(arr1, arr2, num, 0, len(arr1)-1)
+        num = int(math.ceil(float(len(nums1)+len(nums2))/2))
+        if(len(nums1)<=len(nums2)):
+            pos = self.helper(nums1, nums2, num, 0, len(nums1)-1)
             if pos != -1:
-                return arr1[pos]
+                return nums1[pos]
             else:
-                return arr2[self.helper(arr2, arr1, num, 0, len(arr2)-1)]
+                return nums2[self.helper(nums2, nums1, num, 0, len(nums2)-1)]
         else:
-            pos = self.helper(arr2, arr1, num, 0, len(arr2)-1)
+            pos = self.helper(nums2, nums1, num, 0, len(nums2)-1)
             if pos != -1:
-                return arr2[pos]
+                return nums2[pos]
             else:
-                return arr1[self.helper(arr1, arr2, num, 0, len(arr1)-1)]
+                return nums1[self.helper(nums1, nums2, num, 0, len(nums1)-1)]
             
             
     def helper(self, targetArr, searchArr, num, start, end):
-        if start > end:
-            return -1
-        mid = int(math.floor((float(end-start))/2)+start)
-        remainNum = num - (mid + 1)
-        if(remainNum>0):
-            if searchArr[remainNum-1] <= targetArr[mid] and searchArr[remainNum] >= targetArr[mid]:
-                return mid
-            elif searchArr[remainNum-1] > targetArr[mid]:
-                mid = self.helper(targetArr, searchArr, num, mid + 1, end)
-            elif searchArr[remainNum] < targetArr[mid]:
-                mid = self.helper(targetArr, searchArr, num, start, mid - 1)
-        else:
-            if searchArr[remainNum] >= targetArr[mid]:
-                return mid
+            if start > end:
+                return -1
+            mid = int(math.floor((float(end-start))/2)+start)
+            remainNum = num - (mid + 1)
+            if remainNum>0 and remainNum+1<len(searchArr):
+                if searchArr[remainNum-1] <= targetArr[mid] and searchArr[remainNum] >= targetArr[mid]:
+                    return mid
+                elif searchArr[remainNum-1] > targetArr[mid]:
+                    mid = self.helper(targetArr, searchArr, num, mid + 1, end)
+                elif searchArr[remainNum] < targetArr[mid]:
+                    mid = self.helper(targetArr, searchArr, num, start, mid - 1)
+            elif remainNum<=0 and remainNum+1<len(searchArr):
+                if searchArr[remainNum] >= targetArr[mid]:
+                    return mid
+                else:
+                    mid = self.helper(targetArr, searchArr, num, start, mid - 1)
+            elif remainNum+1>=len(searchArr) and remainNum>0:
+                if searchArr[remainNum-1] <= targetArr[mid]:
+                    return mid
+                else:
+                    mid = self.helper(targetArr, searchArr, num, mid + 1, end)
             else:
-                mid = self.helper(targetArr, searchArr, num, start, mid - 1)
-        return mid
+                return -1
+            return mid
 
 if __name__ == "__main__":
     caller = MedianofTwoSortedArray()
-    arr1 = [1,2,3,4,5]
-    arr2 = [3,6,7,90]
+    arr1 = [1]
+    arr2 = [3,6]
     print str(caller.findMedian(arr1, arr2))
