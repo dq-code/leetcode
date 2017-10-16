30a5157ad134c27517bd31829b3e049cbc81db52
qian deng <dengqianwork@gmail.com>
2017-10-15 20:20:43 -0700

    update

--- 54-SpiralMatrix.py
+++ 54-SpiralMatrix.py
@@ -4,22 +4,28 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        if len(matrix) == 0: return []
-        directions = [[0,1], [1,0], [0,-1], [-1,0]]
-        res = []
-        row = col = 0
-        dir = 0
-        while len(res) < len(matrix)*len(matrix[0]):
-            res.append(matrix[row][col])
-            matrix[row][col] = 'x'
-            tempRow = row+directions[dir][0]
-            tempCol = col + directions[dir][1]
-            if tempRow<0 or tempRow>=len(matrix) or tempCol<0 or tempCol>=len(matrix[0]) or matrix[tempRow][tempCol] == 'x':
-                dir += 1
-                dir = dir%4
-                tempRow = row + directions[dir][0]
-                tempCol = col + directions[dir][1]
-            row = tempRow
-            col = tempCol
+        if not matrix or not matrix[0]: return []
 
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        r, c = 0, 0
+        d = 0
+        res = []
+        while True:
+            res.append(matrix[r][c])
+            matrix[r][c] = 'x'
+            visited = [d]
+            all_visited = False
+            while True:
+                nr, nc = r + dirs[d][0], c + dirs[d][1]
+                if nr < 0 or nr >= len(matrix) or nc < 0 or nc >= len(matrix[0]) or matrix[nr][nc] == 'x':
+                    d = (d + 1) % 4
+                    if d not in visited:
+                        visited.append(d)
+                    else:
+                        all_visited = True
+                        break
+                else:
+                    r, c = nr, nc
+                    break
+            if all_visited: break
         return res
